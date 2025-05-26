c9bea87822b79d7812fe91572fb920cdd7039e3b
myroslavSB <myroslav.sobchyshyn@px.com>
2025-05-26 20:53:49 +0200

    created init application

---
+++ src/main/java/com/example/pathfinderbe/PathfinderBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathfinderbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathfinderBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pathfinderbe/PathfinderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathfinderbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
