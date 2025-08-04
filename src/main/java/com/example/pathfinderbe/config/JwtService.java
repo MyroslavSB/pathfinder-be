b86d5d3981b031f8960d3a4b68da3fb8ebf4356c
myroslavSB <myroslav.sobchyshyn@px.com>
2025-08-04 20:12:12 +0200

    created generateToken method

--- src/main/java/com/example/pathfinderbe/config/JwtService.java
+++ src/main/java/com/example/pathfinderbe/config/JwtService.java
@@ -2,12 +2,16 @@ package com.example.pathfinderbe.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -24,6 +28,23 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSingInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     private Claims extractAllClaims(String token) {
         return Jwts
