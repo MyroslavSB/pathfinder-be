6ceef67a9fdeea1b41c272bf414487d0c0532aa6
myroslavSB <myroslav.sobchyshyn@px.com>
2025-08-04 20:05:01 +0200

    implemented extractClaims method

--- src/main/java/com/example/pathfinderbe/config/JwtService.java
+++ src/main/java/com/example/pathfinderbe/config/JwtService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -15,6 +16,11 @@ public class JwtService {
     private static final String SECRET_KEY = "LoDd96qHuBZucxkFWCUIxgQPZPi2kp14";
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
 
