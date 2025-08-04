a068824145991a8786cc5c05f41719a7064f7f11
myroslavSB <myroslav.sobchyshyn@px.com>
2025-08-04 20:02:47 +0200

    implemented extractClaims method

--- src/main/java/com/example/pathfinderbe/config/JwtService.java
+++ src/main/java/com/example/pathfinderbe/config/JwtService.java
@@ -1,11 +1,34 @@
 package com.example.pathfinderbe.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "LoDd96qHuBZucxkFWCUIxgQPZPi2kp14";
     public String extractUsername(String token) {
         return null;
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSingInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
