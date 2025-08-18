58a36057136902529c3a63c017ce5d5b4819c17b
myroslavSB <myroslav.sobchyshyn@px.com>
2025-08-18 22:34:09 +0200

    created register method in AuthenticationService

--- src/main/java/com/example/pathfinderbe/auth/AuthenticationService.java
+++ src/main/java/com/example/pathfinderbe/auth/AuthenticationService.java
@@ -1,14 +1,36 @@
 package com.example.pathfinderbe.auth;
 
+import com.example.pathfinderbe.config.JwtService;
+import com.example.pathfinderbe.user.Role;
+import com.example.pathfinderbe.user.User;
+import com.example.pathfinderbe.user.UserRepository;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
 
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
