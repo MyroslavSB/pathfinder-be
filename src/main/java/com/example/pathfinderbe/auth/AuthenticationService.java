046c6b57c85ae12e46a2ef877cc816edf66a0860
myroslavSB <myroslav.sobchyshyn@px.com>
2025-08-18 22:30:15 +0200

    created AuthenticationService

---
+++ src/main/java/com/example/pathfinderbe/auth/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.example.pathfinderbe.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+    }
+}
