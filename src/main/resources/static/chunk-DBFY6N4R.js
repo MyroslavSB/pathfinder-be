dea563a88b0370dede6060ff816888430a4b247d
myroslavSB <myroslav.sobchyshyn@px.com>
2026-01-21 20:50:22 +0100

    added static angular built files + SpaWebConfig to host them

---
+++ src/main/resources/static/chunk-DBFY6N4R.js
@@ -0,0 +1,1 @@
+import{P as r,Y as o,ga as s,h as n}from"./chunk-PZ3LDO2P.js";function w(t){t||(t=o(s));let i=new n(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(r(i))}export{w as a};
